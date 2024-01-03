3c930305f4be0a3d11fffaf90ef269a1679ee7cd
Swai15 <swaisilvano5@gmail.com>
2024-01-03 14:58:40 +0300

    fetch and display task, task component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React, { useState, useEffect } from "react";
+import Task from "./components/Task";
+
 import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const URL = "http://127.0.0.1:8000/api";
+
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch(URL + "/tasks");
+      const data = await response.json();
+      setTasks(data);
+    } catch (error) {
+      console.error("Error fetching tasks: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
   return (
     <div className="App">
-      <p>Yes</p>
+      <h2>Tasks</h2>
+      <hr />
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
     </div>
   );
 }
